4e5931cc5ae86a238b5ef1451c6bd9dbbb14b366
Artem Iasakov <Artem_Iasakov@epam.com>
2017-12-22 11:26:11 +0400

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getPositiveKeysOfBoolset(boolset) {
-  // your solution here
-}
+    const result = [];
+    for (let i = 0; i < boolset.length - 1; i += 2) {
+        if (boolset[i + 1]) {
+            result.push(boolset[i]);
+        }
+    }
+
+    return result;
+};
